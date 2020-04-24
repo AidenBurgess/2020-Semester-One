9e0f9b2271f5eaf9cd754b2151f73471e1cf606e
Aiden Burgess <aiden9874@gmail.com>
2020-04-25 00:15:49 +1200

    Started ex2q1

--- SOFTENG_364_Networks/Assignments/1/TCPclient_Exercise1.py
+++ SOFTENG_364_Networks/Assignments/1/TCPclient_Exercise1.py
@@ -6,14 +6,14 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 print(f"Connection established to server {HOST}: {PORT}")
 
-with open("output.wav", "wb") as data:
+with open("received.wav", "wb") as data:
     start_time = time.time()
     while True:
         line = s.recv(1024)
         if not line:
             break
         data.write(line)
-
 end_time = time.time()
 s.close()
+print("File transfer complete – file name: audio.wav, saved as received.wav")
 print(f"start: {start_time} end: {end_time} difference: {end_time-start_time}")
