ed085b2b47b7e6688e78a48e51cf7c93007e3fee
Aiden Burgess <aiden9874@gmail.com>
2020-06-26 12:11:21 +1200

    Update exam-prep.py

--- COMPSCI_373_Graphics/exam-prep.py
+++ COMPSCI_373_Graphics/exam-prep.py
@@ -186,3 +186,178 @@ def computeConnectedComponentLabeling(pixel_array, image_width, image_height):
     
     return ccl, counts
     
+
+
+def flipImageAlongHorizontalCenterRow(pixel_array, image_width, image_height):
+    new = createInitializedGreyscalePixelArray(image_width, image_height)
+    
+    for i in range(image_height):
+        for j in range(image_width):
+            new[image_height - i-1][j] = pixel_array[i][j]
+            
+    return new
+
+
+def computeNegationImage(pixel_array, image_width, image_height):
+    new = createInitializedGreyscalePixelArray(image_width, image_height)
+    
+    for i in range(image_height):
+        for j in range(image_width):
+            new[i][j] = 7-pixel_array[i][j]
+            
+    return new
+
+
+def computeBoxAveraging3x3ZeroBorder(pixel_array, image_width, image_height):
+    new = createInitializedGreyscalePixelArray(image_width, image_height)
+    
+    filter_matrix = {
+        (-1, -1): 1, (0, 1): 1, (1, -1): 1,
+        (-1, 0): 1, (0, 0): 1, (1, 0): 1,
+        (-1, 1): 1, (0, -1): 1, (1, 1): 1
+    }
+    
+    for i in range(image_height):
+        for j in range(image_width):
+            total = 0
+            for x,y in filter_matrix:
+                if (0 <= i+y < image_height) and (0 <= j+x < image_width):
+                    total += pixel_array[i+y][j+x] * filter_matrix[(x,y)]
+            new[i][j] = total/9
+    return new
+
+
+def computeBoxAveraging3x3ZeroBorder(pixel_array, image_width, image_height):
+    new = createInitializedGreyscalePixelArray(image_width, image_height)
+    
+    filter_matrix = {
+        (-1, -1): 1, (0, 1): 1, (1, -1): 1,
+        (-1, 0): 1, (0, 0): 1, (1, 0): 1,
+        (-1, 1): 1, (0, -1): 1, (1, 1): 1
+    }
+    
+    for i in range(image_height):
+        for j in range(image_width):
+            total = 0
+            for x,y in filter_matrix:
+                if (0 <= i+y < image_height) and (0 <= j+x < image_width):
+                    total += pixel_array[i+y][j+x] * filter_matrix[(x,y)]
+            new[i][j] = total/9
+    return new
+
+
+def computeDifferenceOfGaussians(pixel_array, image_width, image_height):
+    gaus = createInitializedGreyscalePixelArray(image_width, image_height)
+    filter_matrix = {
+        (-1, -1): 1, (0, 1): 2, (1, -1): 1,
+        (-1, 0): 2, (0, 0): 4, (1, 0): 2,
+        (-1, 1): 1, (0, -1): 2, (1, 1): 1
+    }
+    
+    for i in range(1, image_height-1):
+        for j in range(1, image_width-1):
+            total = 0
+            for x,y in filter_matrix:
+                total += pixel_array[i+y][j+x] * filter_matrix[(x,y)]
+            gaus[i][j] = total/16
+    
+    
+    mean = createInitializedGreyscalePixelArray(image_width, image_height)
+    
+    filter_matrix = {
+        (-1, -1): 1, (0, 1): 1, (1, -1): 1,
+        (-1, 0): 1, (0, 0): 1, (1, 0): 1,
+        (-1, 1): 1, (0, -1): 1, (1, 1): 1
+    }
+    
+    for i in range(1, image_height-1):
+        for j in range(1, image_width-1):
+            total = 0
+            for x,y in filter_matrix:
+                total += pixel_array[i+y][j+x] * filter_matrix[(x,y)]
+            mean[i][j] = total/9
+            gaus[i][j] -= mean[i][j]
+            
+    return gaus
+
+
+def computeHistogramEqualization(pixel_array, image_width, image_height):
+    hist = [0 for i in range(255)]
+    for row in pixel_array:
+        for val in row:
+            hist[val] += 1
+    cum_hist = [0 for i in range(255)]
+    for i, val in enumerate(hist):
+        cum_hist[i] += cum_hist[i-1] + val
+
+    q_min = 0
+    for i,  val in enumerate(cum_hist):
+        if val != 0:
+            q_min = i
+            break
+        
+    q_max = 255
+    for i, val in enumerate(cum_hist):
+        if val == image_width * image_height:
+            q_max = i
+            break
+    
+    T = {}
+    
+    for q in range(255):
+        if q < q_min:
+            T[q] = 0
+        else:
+            T[q] = round(255 * (cum_hist[q] - cum_hist[q_min])/(cum_hist[q_max] - cum_hist[q_min]))
+    
+    for i in range(image_height):
+        for j in range(image_width):
+            pixel_array[i][j] = T[pixel_array[i][j]]
+    return pixel_array
+
+
+def computeHistogram(pixel_array, image_width, image_height, nr_bins = 256):
+    hist = [0.0 for i in range(nr_bins)]
+    
+    for row in pixel_array:
+        for pixel in row:
+            hist[pixel] += 1.0
+            
+    return hist
+
+def computeCumulativeHistogram(pixel_array, image_width, image_height, nr_bins = 256):
+    hist = computeHistogram(pixel_array, image_width, image_height, nr_bins)
+    cum_hist = []
+    
+    total = 0
+    for i, val in enumerate(hist):
+        total += val
+        cum_hist.append(total)
+        
+    return cum_hist
+
+def computeRGBToGreyscale(r, g, b, image_width, image_height):
+    greyscale_pixel_array = createInitializedGreyscalePixelArray(image_width, image_height)
+    # STUDENT CODE HERE
+    for i in range(image_height):
+        for j in range(image_width):
+            greyscale_pixel_array[i][j] = round(0.299*r[i][j]+0.587*g[i][j]+0.114*b[i][j])
+        
+    
+    return greyscale_pixel_array
+
+
+def scaleTo0And255AndQuantize(pixel_array, image_width, image_height):
+    min_val, max_val = computeMinAndMaxValues(pixel_array, image_width, image_height)
+    new = createInitializedGreyscalePixelArray(image_width, image_height)
+    
+    if (min_val == max_val):
+        return new
+    
+    for i in range(image_height):
+        for j in range(image_width):
+            new[i][j] = min(255, max(0, round((pixel_array[i][j]-min_val) *(255/(max_val-min_val)))))
+            
+    return new
+
+
